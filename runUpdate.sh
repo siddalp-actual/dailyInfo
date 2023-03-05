d125b9726d76929662d2d0d626619afc210d03a3
siddalp-actual <76445019+siddalp-actual@users.noreply.github.com>
2023-03-05 13:33:00 +0000

    activation script runs on either machine

--- runUpdate.sh
+++ runUpdate.sh
@@ -1,5 +1,19 @@
 #! /bin/bash
-cd ~/Dropbox/pgm/gkeep
-#. ~/pgm/py3env/bin/activate
-. ~/.pyenv/versions/pyenvProj1/bin/activate
+case `/bin/hostname` in
+	"Apollo14")
+	echo 'running on laptop'
+    cd ~/Dropbox/pgm/gkeep
+    . ~/.pyenv/versions/gnrlPy/bin/activate
+	;;
+
+	"Pi3")
+    cd ~/pgm/gkeep
+	echo 'running on pi'
+    . ~/.pyenv/versions/py3/bin/activate
+	;;
+
+	*)
+	echo `/bin/hostname`
+	;;
+esac
 ./update_note.py
